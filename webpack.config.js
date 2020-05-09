1379f9e3ce965de1eb2b3ea244caf70c864152d0
jimmyfargo <zblizzard@live.com>
2020-05-09 17:35:07 -0500

    using typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// const { InjectManifest } = require("workbox-webpack-plugin");
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-    mode: "production",
-    entry: {
-        main: "./src/index.js",
-        'service-worker': "./src/service-worker.js",
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-    },
-    resolve: { extensions: ['.js', '.jsx'] },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html'),
-        }),
-        // new InjectManifest({
-        //     swSrc: path.join(__dirname, 'src/service-worker.js'),
-        // }),
+  mode: isProd ? "production" : "development",
+  entry: {
+    main: "./src/index.tsx",
+    "service-worker": "./src/service-worker.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  devtool: !isProd && "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+    hot: true,
+  },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
